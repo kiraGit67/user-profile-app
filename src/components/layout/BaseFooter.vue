a50ac7244df0a48537cbb4d1333d1ccca03b5d97
kiraGit67 <kirsten@sais.de>
2023-05-29 20:55:25 +0200

    feat: adds src/components/layout/BaseFooter.vue

---
+++ src/components/layout/BaseFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="columns">
+    <div class="column"><slot name="subtitle-left"></slot></div>
+    <div class="column"><slot name="subtitle-middle"></slot></div>
+    <div class="column"><slot name="subtitle-right"></slot></div>
+  </div>
+</template>
