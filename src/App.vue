becf31ff29493c932a3dc3d560be4c5698cf1700
kiraGit67 <kirsten@sais.de>
2023-05-29 20:55:44 +0200

    imports BaseFooter component into App.vue

--- src/App.vue
+++ src/App.vue
@@ -7,17 +7,24 @@
   <BaseLayout>
     <users-list />
   </BaseLayout>
+  <BaseFooter>
+    <template v-slot:subtitle-left><h3>Users</h3></template>
+    <template v-slot:subtitle-middle><h3>Friends</h3></template>
+    <template v-slot:subtitle-right><h3>Non-Friends</h3></template>
+  </BaseFooter>
 </template>
 
 <script>
 import BaseHeader from "@/components/layout/BaseHeader.vue";
 import BaseLayout from "@/components/layout/BaseLayout.vue";
 import UsersList from "@/components/UsersList.vue";
+import BaseFooter from "@/components/layout/BaseFooter.vue";
 export default {
   components: {
     BaseHeader,
     BaseLayout,
     UsersList,
+    BaseFooter,
   },
 };
 </script>
