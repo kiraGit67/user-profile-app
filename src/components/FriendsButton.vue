6d7ccb1852ab41c805c8f0aae54d2c5d0a908840
kiraGit67 <kirsten@sais.de>
2023-05-29 19:21:04 +0200

    adds src/components/FriendsButton.vue

---
+++ src/components/FriendsButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <button @click="this.$emit('changeFriendsState')">
+    {{
+      friendsState ? "- Als Freund:in entfernen" : "+ als Freund:in hinzufügen"
+    }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    friendsState: Boolean,
+  },
+};
+</script>
